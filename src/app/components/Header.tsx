5e77e8c77c088e3cf8a84e4823112679a58c0dc8
Kepler Jay <keplerjst@gmail.com>
2025-12-11 13:05:02 +0900

    use layer base

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header>
-      <h1 className="text-3xl font-bold">Preact SSR on Cloudflare Workers</h1>
+      <h1>Preact SSR on Cloudflare Workers</h1>
       <div className="flex justify-center gap-2">
         <a className="p-1 hover:bg-gray-100" href="/">
           Home
